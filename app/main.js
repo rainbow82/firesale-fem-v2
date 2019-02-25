32757c181c2cdee7b82a14fc914dac0a8912ee68
Beach, Shannon <shannon.beach@cbsinteractive.com>
2019-02-24 19:22:44 -0500

    created window

--- app/main.js
+++ app/main.js
@@ -1,1 +1,15 @@
-console.log('This is an Electron application!');
+const {app, BrowserWindow} = require('electron');
+
+let mainWindow = null;
+
+//create the window, don't show it til the html file is loaded
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({show: false});
+
+    mainWindow.loadFile(`${__dirname}/index.html`);
+
+    mainWindow.on('ready-to-show', () =>{
+        mainWindow.show();
+    });
+});
+
