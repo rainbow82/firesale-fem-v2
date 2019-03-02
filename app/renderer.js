5503368f4640dec808b8ecc938d7a0bc3f8ecdc1
Beach, Shannon <shannon.beach@cbsinteractive.com>
2019-03-02 08:46:16 -0500

    updated file path

--- app/renderer.js
+++ app/renderer.js
@@ -1,7 +1,12 @@
 const marked = require('marked');
+const path = require('path');
 const {remote, ipcRenderer} = require('electron');
 
+let filePath = null;
+let originalContent = '';
+
 const mainProcess = remote.require('./main');
+const currentWindow = remote.getCurrentWindow();
 
 const markdownView = document.querySelector('#markdown');
 const htmlView = document.querySelector('#html');
@@ -17,6 +22,16 @@ const renderMarkdownToHtml = markdown => {
   htmlView.innerHTML = marked(markdown, { sanitize: true });
 };
 
+const updateUserInterface = () => {
+  let title = 'Fire Sale';
+
+  if(filePath){
+    title = `${path.basename(filePath)} - ${title}`;
+  }
+
+  currentWindow.setTitle(title);
+};
+
 markdownView.addEventListener('keyup', event => {
   const currentContent = event.target.value;
   renderMarkdownToHtml(currentContent);
@@ -27,6 +42,9 @@ openFileButton.addEventListener('click', () =>{
 });
 
 ipcRenderer.on('file-opened', (event, file, content) => {
+  filePath = file;
+  originalContent = content;
   markdownView.value = content;
   renderMarkdownToHtml(content);
+  updateUserInterface();
 });
